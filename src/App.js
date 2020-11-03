919b46e1bf7f433866584285fe291da3e9f46c12
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-03 17:12:00 +0800

    :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { RecoilRoot } from "recoil";
+
+import Label from "./Label";
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	  <RecoilRoot>
+		  <Label />
+		  <Button />
+	  </RecoilRoot>
   );
 }
 
