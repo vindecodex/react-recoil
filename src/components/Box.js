30821efae675a46a36189aa4138c55cae30ed2b4
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-05 14:49:34 +0800

    :tada:

--- src/components/Box.js
+++ src/components/Box.js
@@ -5,6 +5,11 @@ import { checkWinner } from "../validation/checkWinner";
 import { places } from "../states/places";
 import { winner } from "../states/winner";
 import { turn } from "../states/turn";
+import { AImove } from "../ai/decision";
+
+import { playable } from "../actions/gameStatus";
+import { generatePlace } from "../actions/setPlaces";
+
 
 const Box = ({data}) => {
 	// holds the player value and array index
@@ -12,26 +17,28 @@ const Box = ({data}) => {
 	const [_places, setPlaces] = useRecoilState(places);
 	const [_turn, setTurn] = useRecoilState(turn);
 	const [_winner, setWinner] = useRecoilState(winner);
+
 	const onClick = () => {
-		// check if places is already clicked
-		if (item !== "unsigned" || _winner !== "no") return
-		// get a new copy of state
-		const newPlaces = [..._places];
-		// replace the specific index with player value
-		newPlaces[index] = _turn;
-		// updated the state
-		setPlaces([...newPlaces]);
-		// changing turns
-		if (_turn === "P1")
+		if (!playable(item,_winner)) return
+		let newPlaces = generatePlace(_places,index,_turn);
+		setPlaces(newPlaces)
+		if (_turn === "P1") {
 			setTurn("P2");
-		else
+			newPlaces = generatePlace(newPlaces,AImove(newPlaces),"P2");
+			setPlaces(newPlaces)
 			setTurn("P1");
-		if (checkWinner(newPlaces, _turn)) {
-			setWinner(_turn);
 		}
+		else {
+			setTurn("P1");
+		}
+		if (checkWinner(newPlaces, _turn))
+			setWinner(_turn);
+
 	}
+
 	return (
 		<div className={`box ${item}`} onClick={() => onClick()}>
+			{ index }
 		</div>
 	)
 }
