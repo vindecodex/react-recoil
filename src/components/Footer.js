5c6feece4f1caf29bb4c0974c2603a34f907e78d
Vincent Villaluna <villalunavincent@gmail.com>
2020-11-06 11:58:04 +0800

    :heavy_plus_sign:

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+	return (
+		<div className="footer">
+			<a href="https://github.com/vindecodex/react-recoil">Open in Github</a>
+		</div>
+	)
+}
+
+export default Footer;
